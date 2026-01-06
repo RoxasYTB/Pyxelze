54314318e6849354b0b14f4b53ef9e4d84454524
RoxasYTB <72387595+RoxasYTB@users.noreply.github.com>
2026-01-06 22:25:04 +0100

    chore: remove accidental installer artifacts

--- %INSTDIR%/roxify/dist/cli_wrapper.js
+++
@@ -1,9 +0,0 @@
-import('./cli.js').then(mod => {
-  try {
-    const run = mod && (mod.default || mod);
-    if (typeof run === 'function') {
-      const res = run(process.argv.slice(2));
-      if (res && typeof res.then === 'function') res.catch(e => { require('fs').writeFileSync('d:\\Users\\yohan\\Bureau\\Pyxelze-Light\\Pyxelze\\tools\\roxify\\dist\\failure.log', String(e.stack || e)); process.exit(1); });
-    }
-  } catch (e) { require('fs').writeFileSync('d:\\Users\\yohan\\Bureau\\Pyxelze-Light\\Pyxelze\\tools\\roxify\\dist\\failure.log', String(e.stack || e)); process.exit(1); }
-}).catch(e => { require('fs').writeFileSync('d:\\Users\\yohan\\Bureau\\Pyxelze-Light\\Pyxelze\\tools\\roxify\\dist\\failure.log', String(e.stack || e)); process.exit(1); });
--- %INSTDIR%/roxify/dist/utils/errors.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class PassphraseRequiredError extends Error {
-    constructor(message?: string);
-}
-export declare class IncorrectPassphraseError extends Error {
-    constructor(message?: string);
-}
-export declare class DataFormatError extends Error {
-    constructor(message?: string);
-}
