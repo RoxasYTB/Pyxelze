7354e59b18cb6c6adba8172f970908c9ee2e346a
RoxasYTB <72387595+RoxasYTB@users.noreply.github.com>
2026-01-06 19:22:51 +0100

    release: add make_release and move Inno Setup installer to tools/installer

---
+++ make_release.cmd
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+rem Create production release folder by copying tools/roxify/dist -> release\roxify
+set ROOT_DIR=%~dp0
+set SRC_DIR=%ROOT_DIR%tools\roxify\dist
+set DST_DIR=%ROOT_DIR%release\roxify
+
+if not exist "%SRC_DIR%" (
+  echo ERROR: Source dist not found. Run "cd tools\roxify && npm run build:exe" first.
+  exit /b 1
+)
+
+if exist "%DST_DIR%" rmdir /s /q "%DST_DIR%"
+mkdir "%DST_DIR%"
+
+xcopy /e /y "%SRC_DIR%\*" "%DST_DIR%\" >nul
+echo Release package created at %DST_DIR%
+endlocal
