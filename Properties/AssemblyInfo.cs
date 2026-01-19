09c415c6b65957980af86b048d47b96869144262
RoxasYTB <roxasyotabe@gmail.com>
2026-01-19 10:22:25 +0100

    Add scripts for binary checks, self-signed certificate generation, and false-positive submission preparation
    - Created `check_binaries.sh` to scan directories for suspicious binaries, checking for UPX markers, version info, and other heuristics.
    - Added `create_self_signed_pfx.sh` to generate a self-signed PFX certificate for code signing.
    - Implemented `prepare_fp_submission.sh` to prepare a package for false-positive submissions, including a report and tarball of relevant files.
    - Added `SHA256SUMS.txt` to store the SHA256 checksum of the release zip file.
    - Included a self-signed PFX certificate file for code signing.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Pyxelze")]
 [assembly: AssemblyProduct("Pyxelze")]
-[assembly: AssemblyCompany("Pyxelze")] 
+[assembly: AssemblyCompany("Pyxelze")]
 [assembly: AssemblyDescription("Pyxelze - file archiver and tools")]
-[assembly: AssemblyCopyright("Copyright © Pyxelze 2026")] 
+[assembly: AssemblyCopyright("Copyright © Pyxelze 2026")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
