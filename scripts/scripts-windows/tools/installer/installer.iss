71a50a730b33ff7d4e6238aad7f2c62465dbb06e
RoxasYTB <roxasyotabe@gmail.com>
2026-03-22 21:19:07 +0100

    v1.3.1: bump version, roxify 1.9.2 with fixed .node loading

--- scripts/scripts-windows/tools/installer/installer.iss
+++ scripts/scripts-windows/tools/installer/installer.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Pyxelze"
-#define MyAppVersion "1.3.0"
+#define MyAppVersion "1.3.1"
 #define MyAppPublisher "Pyxelze"
 #define MyAppURL "https://github.com/RoxasYTB/Pyxelze"
 #define MyAppExeName "Pyxelze.exe"
--- src/Helpers/UpdateChecker.cs
+++ src/Helpers/UpdateChecker.cs
@@ -8,7 +8,7 @@ namespace Pyxelze;
 internal static class UpdateChecker
 {
     private const string GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
-    private const string CurrentVersion = "1.3.0";
+    private const string CurrentVersion = "1.3.1";
 
     private static readonly HttpClient _httpClient = new()
     {
