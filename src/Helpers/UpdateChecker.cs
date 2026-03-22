2261bf578f1ba84c68d3be0bc31a1131367e4d6b
RoxasYTB <roxasyotabe@gmail.com>
2026-03-22 16:00:56 +0100

    v1.2.4: Grayed up button, empty archive on startup, add button, version bump

--- src/Helpers/UpdateChecker.cs
+++ src/Helpers/UpdateChecker.cs
@@ -8,7 +8,7 @@ namespace Pyxelze;
 internal static class UpdateChecker
 {
     private const string GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
-    private const string CurrentVersion = "1.2.3";
+    private const string CurrentVersion = "1.2.4";
 
     private static readonly HttpClient _httpClient = new()
     {
