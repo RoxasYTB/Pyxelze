c297640b31f8bf769b5cdb2206597b47cfb6e52c
RoxasYTB <roxasyotabe@gmail.com>
2026-03-24 15:38:36 +0100

    v1.2.3: cascading context menu, CLI extract/encode actions, Linux compat, installer integration

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -2,8 +2,8 @@
 #include <QString>
 
 namespace AppConstants {
-    inline constexpr const char* Version = "1.2.2";
-    inline constexpr const char* BuildStamp = "20260324-180000";
+    inline constexpr const char* Version = "1.2.3";
+    inline constexpr const char* BuildStamp = "20260324-200000";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
 #include <QApplication>
 #include <QFile>
+#include <QFileInfo>
+#include <QFileDialog>
+#include <QDir>
 #include "core/AppConstants.h"
 #include "localization/Localization.h"
+#include "archive/ExtractionService.h"
+#include "archive/CompressionService.h"
 #include "ui/MainWindow.h"
+
+static int runExtractHere(const QString& filePath) {
+    L::init();
+    auto info = QFileInfo(filePath);
+    auto destDir = info.absolutePath() + QLatin1Char('/') + info.completeBaseName();
+    QDir().mkpath(destDir);
+    ExtractionService::extractWithProgress(nullptr, filePath, destDir);
+    return 0;
+}
+
+static int runExtractTo(const QString& filePath) {
+    L::init();
+    auto dir = QFileDialog::getExistingDirectory(nullptr, L::get("toolbar.extractAll"));
+    if (dir.isEmpty()) return 0;
+    auto destDir = dir + QLatin1Char('/') + QFileInfo(filePath).completeBaseName();
+    QDir().mkpath(destDir);
+    ExtractionService::extractWithProgress(nullptr, filePath, destDir);
+    return 0;
+}
+
+static int runEncode(const QString& filePath) {
+    L::init();
+    CompressionService::compressDirectory(nullptr, filePath);
+    return 0;
+}
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
@@ -10,16 +40,27 @@ int main(int argc, char* argv[]) {
     app.setApplicationVersion(QString::fromLatin1(AppConstants::Version));
     app.setOrganizationName(QStringLiteral("Pyxelze"));
 
-    L::init();
-
+    QString action;
     QString fileToOpen;
     for (int i = 1; i < argc; ++i) {
         auto arg = QString::fromLocal8Bit(argv[i]);
-        if (!arg.startsWith('-') && QFile::exists(arg)) {
+        if (arg == QStringLiteral("--extract-here") || arg == QStringLiteral("--extract-to") || arg == QStringLiteral("--encode")) {
+            action = arg;
+        } else if (!arg.startsWith('-')) {
             fileToOpen = arg;
-            break;
         }
     }
+
+    if (!action.isEmpty() && !fileToOpen.isEmpty()) {
+        if (action == QStringLiteral("--extract-here"))
+            return runExtractHere(fileToOpen);
+        if (action == QStringLiteral("--extract-to"))
+            return runExtractTo(fileToOpen);
+        if (action == QStringLiteral("--encode"))
+            return runEncode(fileToOpen);
+    }
+
+    L::init();
 
     MainWindow w(fileToOpen);
     w.show();
