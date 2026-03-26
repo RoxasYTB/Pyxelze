aecda3b94c155c6e638c2e6727c063c7a651c3e5
RoxasYTB <72387595+RoxasYTB@users.noreply.github.com>
2026-03-26 13:44:35 +0100

    v1.2.5: upgrade roxify engine to v1.11.0
    - Roxify v1.11.0: interleaved rANS, best-of BWT/zstd per block
    - Encode: 89 MB/s (+13%), Decode: 200 MB/s (+41%)
    - Cross-platform roxify binaries included

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -2,7 +2,7 @@
 #include <QString>
 
 namespace AppConstants {
-    inline constexpr const char* Version = "1.2.4";
+    inline constexpr const char* Version = "1.2.5";
     inline constexpr const char* BuildStamp = "20260324-240000";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
