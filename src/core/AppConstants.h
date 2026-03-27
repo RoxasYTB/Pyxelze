0cec37b30d5985e49e9456edd2ebbec5e4bd3f40
RoxasYTB <roxasyotabe@gmail.com>
2026-03-27 22:30:13 +0100

    fix: licence multilingue installateur + buildstamp update

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -3,7 +3,7 @@
 
 namespace AppConstants {
     inline constexpr const char* Version = "1.2.8";
-    inline constexpr const char* BuildStamp = "20260327-192500";
+    inline constexpr const char* BuildStamp = "20260327-220000";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
 }
