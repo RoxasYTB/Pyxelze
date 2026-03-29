ed7b3087ccd051708c622ebe346efab2254af5cc
RoxasYTB <roxasyotabe@gmail.com>
2026-03-29 14:54:32 +0200

    release: Pyxelze v1.3.0 - roxify 1.12.3, fix copyright, optimized compression

--- src/ui/AboutDialog.cpp
+++ src/ui/AboutDialog.cpp
@@ -85,7 +85,7 @@ AboutDialog::AboutDialog(QWidget* parent) : QDialog(parent) {
     lnk->setOpenExternalLinks(true);
     layout->addWidget(lnk);
 
-    auto* lblCopy = new QLabel(L::get("about.copyright").replace(QStringLiteral("{0}"), QStringLiteral("2024-2026")));
+    auto* lblCopy = new QLabel(L::get("about.copyright").replace(QStringLiteral("{0}"), QStringLiteral("2026")));
     lblCopy->setStyleSheet(QStringLiteral("color: %1;").arg(dim.name()));
     layout->addWidget(lblCopy);
 
