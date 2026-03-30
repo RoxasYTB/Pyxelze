171c29a9839d5126a990d63046f007abd8406982
RoxasYTB <roxasyotabe@gmail.com>
2026-03-30 15:36:16 +0200

    release: align Pyxelze 1.3.1 pipeline with roxify 1.12.5

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -3,7 +3,8 @@
 
 namespace AppConstants {
     inline constexpr const char* Version = "1.3.1";
-    inline constexpr const char* BuildStamp = "20250715-120000";
+    inline constexpr const char* BuildStamp = "20260330-150500";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
 }
+
