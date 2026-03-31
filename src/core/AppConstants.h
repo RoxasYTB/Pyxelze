650e0e58335ea9dd407ed0422ff8a606e02babba
RoxasYTB <roxasyotabe@gmail.com>
2026-03-31 12:58:29 +0200

    bump: v1.3.3 with roxify 1.12.9 (MARKER_END fix)

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -2,7 +2,7 @@
 #include <QString>
 
 namespace AppConstants {
-    inline constexpr const char* Version = "1.3.2";
+    inline constexpr const char* Version = "1.3.3";
     inline constexpr const char* BuildStamp = "20260327-220000";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
