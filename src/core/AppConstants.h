c38861c9a7cb986772df56ae8923df60d4cc7588
RoxasYTB <roxasyotabe@gmail.com>
2026-04-02 01:06:05 +0200

    Fix AppConstants version to 1.3.4 and improve license prompt UI

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -2,8 +2,8 @@
 #include <QString>
 
 namespace AppConstants {
-    inline constexpr const char* Version = "1.3.3";
-    inline constexpr const char* BuildStamp = "20260327-220000";
+    inline constexpr const char* Version = "1.3.4";
+    inline constexpr const char* BuildStamp = "20260402-120000";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
 }
