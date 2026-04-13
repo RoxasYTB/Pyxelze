2211f11ea3d91b1f810d8e57178f13a92914973c
RoxasYTB <72387595+RoxasYTB@users.noreply.github.com>
2026-04-13 18:04:49 +0200

    fix: update AppConstants version to 1.3.6

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -2,8 +2,8 @@
 #include <QString>
 
 namespace AppConstants {
-    inline constexpr const char* Version = "1.3.5";
-    inline constexpr const char* BuildStamp = "20260402-120000";
+    inline constexpr const char* Version = "1.3.6";
+    inline constexpr const char* BuildStamp = "20260413-170000";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
 }
