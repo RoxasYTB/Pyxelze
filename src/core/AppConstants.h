c52d070e9a0098f359d22ac7f336d6966b2bd252
RoxasYTB <72387595+RoxasYTB@users.noreply.github.com>
2026-04-14 13:21:24 +0200

    v1.3.7: upgrade roxify v1.13.3, faster encode/decode, fix V8 crash

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -2,8 +2,8 @@
 #include <QString>
 
 namespace AppConstants {
-    inline constexpr const char* Version = "1.3.6";
-    inline constexpr const char* BuildStamp = "20260413-170000";
+    inline constexpr const char* Version = "1.3.7";
+    inline constexpr const char* BuildStamp = "20260414-120000";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
 }
