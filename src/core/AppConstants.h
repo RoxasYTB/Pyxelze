3c6cd57f2c3dd3fa29c32cb8487374deda4f7bb6
RoxasYTB <72387595+RoxasYTB@users.noreply.github.com>
2026-04-16 00:13:09 +0200

    release: pyxelze v1.3.9

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -2,8 +2,8 @@
 #include <QString>
 
 namespace AppConstants {
-    inline constexpr const char* Version = "1.3.8";
-    inline constexpr const char* BuildStamp = "20260415-152319";
+    inline constexpr const char* Version = "1.3.9";
+    inline constexpr const char* BuildStamp = "20260415-234448";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
 }
