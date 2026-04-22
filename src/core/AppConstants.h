b35efc9f84aa1ae9625a0fc2f25712560e65ef97
RoxasYTB <72387595+RoxasYTB@users.noreply.github.com>
2026-04-22 21:29:50 +0200

    feat: Pyxelze 1.4.0 - Performance Revolution

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -2,8 +2,8 @@
 #include <QString>
 
 namespace AppConstants {
-    inline constexpr const char* Version = "1.3.9";
-    inline constexpr const char* BuildStamp = "20260415-234448";
+    inline constexpr const char* Version = "1.4.0";
+    inline constexpr const char* BuildStamp = "20260422-203500";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
 }
