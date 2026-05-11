9ebc82ce7ae1fdb828893e6884bf4231ecb60711
RoxasYTB <72387595+RoxasYTB@users.noreply.github.com>
2026-05-12 00:22:43 +0200

    chore: update About version to 1.4.1

--- src/core/AppConstants.h
+++ src/core/AppConstants.h
@@ -2,8 +2,8 @@
 #include <QString>
 
 namespace AppConstants {
-    inline constexpr const char* Version = "1.4.0";
-    inline constexpr const char* BuildStamp = "20260422-203500";
+    inline constexpr const char* Version = "1.4.1";
+    inline constexpr const char* BuildStamp = "20260512-002100";
     inline constexpr const char* RepoUrl = "https://github.com/RoxasYTB/Pyxelze";
     inline constexpr const char* GitHubApiUrl = "https://api.github.com/repos/RoxasYTB/Pyxelze/releases/latest";
 }
